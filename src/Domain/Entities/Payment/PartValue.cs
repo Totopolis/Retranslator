780995044b89541d672228b826fa022dcc1fed5b
Konstantin Anikeev <anikeew@gmail.com>
2024-06-24 02:47:04 +0300

    json request handmade parser

---
+++ src/Domain/Entities/Payment/PartValue.cs
@@ -0,0 +1,109 @@
+﻿using Domain.Primitives;
+using Domain.Shared;
+using System.Text.Json;
+
+namespace Domain.Entities.Payment;
+
+public sealed class PartValue : ValueObject
+{
+    private PartValue(
+        string agreementNumber,
+        string accountNumber,
+        double amount,
+        string currency)
+    {
+        AggreementNumber = agreementNumber;
+        AccountNumber = accountNumber;
+        Amount = amount;
+        Currency = currency;
+    }
+
+    public string AggreementNumber { get; private set; }
+
+    public string AccountNumber { get; private set; }
+
+    public double Amount { get; private set; }
+
+    public string Currency { get; private set; }
+
+    public static Result<PartValue> Create(JsonElement jsonElement)
+    {
+        // 1. agreementNumber
+        if (!jsonElement.TryGetProperty("agreementNumber", out var agreementNumberElement))
+        {
+            return Result.Failure<PartValue>(new Error(
+                code: "PartValue.Create.TryGetAgreementNumberProperty",
+                message: "TryGetAgreementNumberProperty"));
+        }
+
+        var aggreementNumberValue = agreementNumberElement.GetString();
+        if (string.IsNullOrWhiteSpace(aggreementNumberValue))
+        {
+            return Result.Failure<PartValue>(new Error(
+                code: "PartValue.Create.TryGetAgreementNumberValue",
+                message: "TryGetAgreementNumberValue"));
+        }
+
+        // 2. accountNumber
+        if (!jsonElement.TryGetProperty("accountNumber", out var accountNumberElement))
+        {
+            return Result.Failure<PartValue>(new Error(
+                code: "PartValue.Create.TryGetAccountNumberProperty",
+                message: "TryGetAccountNumberProperty"));
+        }
+
+        var accountNumberValue = accountNumberElement.GetString();
+        if (string.IsNullOrWhiteSpace(accountNumberValue))
+        {
+            return Result.Failure<PartValue>(new Error(
+                code: "PartValue.Create.TryGetAccountNumberValue",
+                message: "TryGetAccountNumberValue"));
+        }
+
+        // 3. amount
+        if (!jsonElement.TryGetProperty("amount", out var amountElement))
+        {
+            return Result.Failure<PartValue>(new Error(
+                code: "PartValue.Create.TryGetAmountProperty",
+                message: "TryGetAmountProperty"));
+        }
+
+        if (!amountElement.TryGetDouble(out var amountValue) || amountValue < 0)
+        {
+            return Result.Failure<PartValue>(new Error(
+                code: "PartValue.Create.TryGetAmountValue",
+                message: "TryGetAmountValue"));
+        }
+
+        // 4. currency
+        if (!jsonElement.TryGetProperty("currency", out var currencyElement))
+        {
+            return Result.Failure<PartValue>(new Error(
+                code: "PartValue.Create.TryCurrencyProperty",
+                message: "TryCurrencyProperty"));
+        }
+
+        var currencyValue = currencyElement.GetString();
+        // TODO: need buisness rules check (810)
+        if (string.IsNullOrWhiteSpace(currencyValue) || currencyValue != "810")
+        {
+            return Result.Failure<PartValue>(new Error(
+                code: "PartValue.Create.TryCurrencyValue",
+                message: "TryCurrencyValue"));
+        }
+
+        return new PartValue(
+            aggreementNumberValue,
+            accountNumberValue,
+            amountValue,
+            currencyValue);
+    }
+
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return AggreementNumber;
+        yield return AccountNumber;
+        yield return Amount;
+        yield return Currency;
+    }
+}
--- src/Domain/Entities/Payment/Payment.cs
+++ src/Domain/Entities/Payment/Payment.cs
@@ -1,14 +1,32 @@
 ﻿using Domain.Primitives;
 using Domain.Shared;
+using System.Text.Json;
 using System.Xml;
 
 namespace Domain.Entities.Payment;
 
 public sealed class Payment : Entity<PaymentId>
 {
-    private Payment(PaymentId id) : base(id)
+    private Payment(
+        PaymentId id,
+        ulong requestId,
+        PartValue debit,
+        PartValue credit,
+        string details) : base(id)
     {
+        RequestId = requestId;
+        Details = details;
+        Debit = debit;
+        Credit = credit;
     }
+
+    public ulong RequestId { get; private set; }
+
+    public PartValue Debit { get; private set; }
+
+    public PartValue Credit { get; private set; }
+
+    public string Details { get; private set; }
 
     public Result<XmlDocument> ConvertToXmlDocument()
     {
@@ -18,6 +36,107 @@ public sealed class Payment : Entity<PaymentId>
 
     public static Result<Payment> Create(JsonRequest.JsonRequest request)
     {
-        return default!;
+        // 1. Check json validity
+        if (!IsJsonValid(request.Content, out var doc))
+        {
+            return Result.Failure<Payment>(new Error(
+                code: "Payment.Create.IsJsonValid",
+                message: "Json request content is not valide json object"));
+        }
+
+        // 2. Extract requestId
+        if (!doc.RootElement.TryGetProperty("request", out var requestElement))
+        {
+            return Result.Failure<Payment>(new Error(
+                code: "Payment.Create.TryGetRequestProperty",
+                message: "Request property not found"));
+        }
+
+        if (!requestElement.TryGetProperty("id", out var requestIdElement))
+        {
+            return Result.Failure<Payment>(new Error(
+                code: "Payment.Create.TryGetRequestIdProperty",
+                message: "Request id property not found"));
+        }
+
+        if (!requestIdElement.TryGetUInt64(out var requestIdValue))
+        {
+            return Result.Failure<Payment>(new Error(
+                code: "Payment.Create.TryGetRequestIdValue",
+                message: "Bad request id value"));
+        }
+
+        // 3. Extract details
+        if (!doc.RootElement.TryGetProperty("details", out var detailsElement))
+        {
+            return Result.Failure<Payment>(new Error(
+                code: "Payment.Create.TryGetDetailsProperty",
+                message: "Request details property not found"));
+        }
+
+        var detailsValue = detailsElement.GetString();
+        // TODO: need check buisness condition
+        if (string.IsNullOrWhiteSpace(detailsValue))
+        {
+            return Result.Failure<Payment>(new Error(
+                code: "Payment.Create.TryGetDetailsValue",
+                message: "Bad request details value"));
+        }
+
+        // 4. Extract debit part
+        if (!doc.RootElement.TryGetProperty("debitPart", out var debitPartElement))
+        {
+            return Result.Failure<Payment>(new Error(
+                code: "Payment.Create.TryGetDebitPartProperty",
+                message: "Request debit part property not found"));
+        }
+
+        var debitPart = PartValue.Create(debitPartElement);
+
+        if (debitPart.IsFailure)
+        {
+            return Result.Failure<Payment>(new Error(
+                code: "Payment.Create." + debitPart.Error.Code,
+                message: debitPart.Error.Message));
+        }
+
+        // 5. Extract credit part
+        if (!doc.RootElement.TryGetProperty("creditPart", out var creditPartElement))
+        {
+            return Result.Failure<Payment>(new Error(
+                code: "Payment.Create.TryGetCreditPartProperty",
+                message: "Request credit part property not found"));
+        }
+
+        var creditPart = PartValue.Create(creditPartElement);
+
+        if (creditPart.IsFailure)
+        {
+            return Result.Failure<Payment>(new Error(
+                code: "Payment.Create." + creditPart.Error.Code,
+                message: creditPart.Error.Message));
+        }
+
+        return new Payment(
+            new PaymentId(Guid.NewGuid()),
+            requestIdValue,
+            debitPart.Value,
+            creditPart.Value,
+            detailsValue);
+    }
+
+    private static bool IsJsonValid(string jsonContent, out JsonDocument doc)
+    {
+        doc = default!;
+
+        try
+        {
+            doc = JsonDocument.Parse(jsonContent);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
     }
 }
